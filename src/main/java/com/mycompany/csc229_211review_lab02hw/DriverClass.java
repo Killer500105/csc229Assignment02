3fac05f5e128cb923192d966b4e0a7cfa0c52ade
Edward Sotelo <110614419+Killer500105@users.noreply.github.com>
2025-09-10 15:00:26 -0400

    DriverClass.java

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -1,28 +1,30 @@
-package com.mycompany.csc229_211review_lab02hw;
-
 /**
  *
  * @author MoaathAlrajab
  */
-public class DriverClass {
-	
-	public static void main(String[] args) {
-		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-		
-		// ToDo 7: Add a toString method for Student class
-		
-		Student std1= new Student("James", 20);
-		
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-		
-		System.out.println(std1);
-		
-		// ToDo 9: add comments and explain your code
-		
-		// ToDo 10: submit using a pull request.
-	}
 
+import java.util.Scanner;
+
+/**
+ * DriverClass is the entry point of the program.
+ * It creates a Student object, takes GPA input from the user,
+ * and prints the student details.
+ */
+public class DriverClass {
+
+    public static void main(String[] args) {
+        // Create a new Student object with a name and age
+        Student std1 = new Student("James", 20);
+
+        // Create a Scanner object to take GPA input from the user
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter GPA for " + std1.getName() + ": ");
+
+        // Read GPA input from the user and set it in the Student object
+        double gpa = sc.nextDouble();
+        std1.setGpa(gpa);
+
+        // Print the Student object details (uses the overridden toString method)
+        System.out.println(std1);
+    }
 }
