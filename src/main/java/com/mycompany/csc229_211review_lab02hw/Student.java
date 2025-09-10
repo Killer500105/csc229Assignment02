dfff74dd7d5e524a38e23f8669fdb124bf6fe3f6
Edward Sotelo <110614419+Killer500105@users.noreply.github.com>
2025-09-10 15:01:17 -0400

    Student.java

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,13 +4,43 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
 
+/**
+ *
+ * Student class extends Person and adds GPA field.
+ */
+public class Student extends Person {
+    private double gpa; // GPA field
+
+    // Constructor
+    public Student(String name, int age) {
+        // Person constructor takes (String, short)
+        super(name, (short) age);
+    }
+
+    // Getter and setter for GPA
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    // Override toString for nice print output
+    @Override
+    public String toString() {
+        return "Student{name=" + getName() + ", age=" + getAge() + ", GPA=" + gpa + "}";
+    }
+
+    // Implement abstract methods from Person
+    @Override
+    public String getAddress() {
+        return "Address not set"; // Stub
+    }
+
+    @Override
+    public void setAddress(String address) {
+        // In a real program, you would store this in a field
+    }
 }
